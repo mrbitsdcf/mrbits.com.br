2c0a8a57cf1523309f87717d2e0c839aab83e938
MrBiTs <mrbits.dcf@gmail.com>
2017-12-18 13:11:30 -0200

    Changing GA

--- static/js/ga.js
+++ static/js/ga.js
@@ -1,8 +1,8 @@
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-106751155-1"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-4159326-1"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments)};
   gtag('js', new Date());
 
-  gtag('config', 'UA-106751155-1');
+  gtag('config', 'UA-4159326-1');
 </script>
